f025148256895498917010c5548fed05643d2000
NereaPinol33 <nereapinol@iesebre.com>
2025-02-16 11:21:41 +0100

    Corregir testedBy a VideosController

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -7,17 +7,6 @@ use App\Models\Video;
 class VideosController extends Controller
 {
     /**
-     * Return the tester's name.
-     *
-     * @param  string  $tester
-     * @return string
-     */
-    public function testedBy($tester)
-    {
-        return "The tester is $tester";
-    }
-
-    /**
      * Show the video by ID.
      *
      * @param  int  $id
@@ -29,5 +18,15 @@ class VideosController extends Controller
         $embedUrl = str_replace('watch?v=', 'embed/', $video->url);
 
         return view('video.show', compact('video', 'embedUrl'));
+    }
+
+    /**
+     * Return the class of the test that tests this controller.
+     *
+     * @return \Tests\Unit\VideoTest
+     */
+    public function testedBy()
+    {
+        return \Tests\Unit\VideosTest::class;
     }
 }
